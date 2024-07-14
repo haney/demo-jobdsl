b9f60918d460904e9c00c70253afca36fe8e8f80
David Haney <dhaney@perforce.com>
2024-07-14 14:10:32 -0700

    Establishing seed script

---
+++ seed.groovy
@@ -0,0 +1,18 @@
+PACKAGES = [
+    demo-package1,
+    demo-package2,
+]
+
+VIEWS = [{
+    name: 'CENTOS6',
+}]
+
+folder('EL') {
+    folder(JENKINS_BRANCH) {
+        for (view in VIEWS) {
+            listView(view.name) {
+                recruse()
+            }
+        }
+    }
+}
