bf992d9b77a9e3a9a993fefe452a023ef05b438c
David Haney <dhaney@perforce.com>
2024-07-15 09:20:36 -0700

    Fixup

--- seed.groovy
+++ seed.groovy
@@ -46,7 +46,7 @@ for (jobName in PACKAGES) {
                                 // ID of the repository, such as origin, to uniquely identify this repository among other remote repositories.
                                 name("origin")
                                 // A refspec controls the remote refs to be retrieved and how they map to local refs.
-                                //refspec("+refs/heads/${BRANCH_NAME}:refs/remotes/origin/${BRANCH_NAME}")
+                                refspec("+refs/heads/${BRANCH_NAME}:refs/remotes/origin/${BRANCH_NAME}")
                             }
                         }
                         matchBranches(false)
