fba8f5bec593f3dd5313a5a479c0fa072827eb88
David Haney <dhaney@perforce.com>
2024-07-15 07:28:57 -0700

    Remote Jenkinsfile

--- seed.groovy
+++ seed.groovy
@@ -29,5 +29,40 @@ for (jobName in PACKAGES) {
                 }
             }
         }
+        factory {
+            remoteJenkinsFileWorkflowBranchProjectFactory {
+                // Relative location within the checkout of your Pipeline script.
+                remoteJenkinsFile("package.jenkinsfile")
+                // localMarker('')
+                remoteJenkinsFileSCM {
+                    // The git plugin provides fundamental git operations for Jenkins projects.
+                    gitSCM {
+                        // Specify the repository to track.
+                        userRemoteConfigs {
+                            userRemoteConfig {
+                                // Specify the URL or path of the git repository.
+                                url('https://github.com/haney/demo-jobdsl.git')
+                                // credentialsId('om-github-access-token')
+                                // ID of the repository, such as origin, to uniquely identify this repository among other remote repositories.
+                                // name("${scmBranch}")
+                                // A refspec controls the remote refs to be retrieved and how they map to local refs.
+                                // refspec("+refs/heads/${scmBranch}:refs/remotes/origin/${scmBranch}")
+                            }
+                        }
+                        // matchBranches(false)
+                        // fallbackBranch("${scmBranch}")
+                        // lookupInParameters(false)
+
+                        // List of branches to build.
+                        branches {
+                            branchSpec {
+                                // Specify the branches if you'd like to track a specific branch in a repository.
+                                name(BRANCH_NAME)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
